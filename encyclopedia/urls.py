6b0b84dd25610e87d45cb8942fff4d3423a37b1d
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-09-13 18:06:14 +0200

    some gitignore stuff

--- encyclopedia/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("/create_page", views.create_page, name="create_page"),
-    path("<str:content_name>", views.content, name="content")
-]
