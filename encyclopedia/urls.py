38e3e4a44b66735f6fe2bc7caf50c653a26143a9
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-09-15 23:16:33 +0200

    Added edit_page and random_page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("create_page", views.create_page, name="create_page"),
-    path("<str:content_name>", views.content, name="content")
+    path("random_page", views.random_page, name="random_page"),
+    path("<str:content_name>", views.content, name="content"),
+    path("<str:content_name>/edit_page", views.edit_page, name="edit_page"),
+
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django import forms
-
+from django.urls import reverse
+import random
+import markdown2
 from . import util
 
 class CreatePageForm(forms.Form):
@@ -29,9 +31,10 @@ def content(request,content_name):
             'content_name':content_name,
         })
     else:
+        content_markdown = markdown2.markdown(content)
         return render(request,'encyclopedia/content.html',{
-            'content_name':content_name.capitalize(),
-            'content':content,
+            'content_name':content_name,
+            'content':content_markdown,
         })
     
 def create_page(request):
@@ -58,3 +61,43 @@ def create_page(request):
     return render(request,'encyclopedia/create_page.html',{
         'create_page_form':CreatePageForm()
         })
+
+def edit_page(request,content_name):
+    content_name_safe=util._get_name(content_name)   
+    
+    if content_name_safe is None:
+        return render(request,'encyclopedia/404_error.html',{
+            'content_name':content_name,
+        })
+    del content_name
+
+    content = util.get_entry(content_name_safe)
+
+    if request.method=='POST':
+        form = CreatePageForm(request.POST)
+        if not form.is_valid():
+            return render(request,'encyclopedia/edit_page.html',{
+                'error':'Invalid input',
+                'create_page_form':form
+            })
+
+        util.save_entry(content_name_safe,form.cleaned_data['content'])
+        return HttpResponseRedirect(reverse(f'content',args=[content_name_safe]))
+
+
+    initial_data = {
+        'name': content_name_safe,
+        'content': content
+    }
+
+    edit_form = CreatePageForm(initial=initial_data)
+    edit_form.fields['name'].widget.attrs['disabled'] = 'disabled'
+    
+
+    return render(request,'encyclopedia/edit_page.html',{
+        'create_page_form':edit_form
+        })
+
+def random_page(request):
+    return HttpResponseRedirect(reverse('content',args=[random.choice(util.list_entries())]))
+
