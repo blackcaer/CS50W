2a12e4fda79c9b06d33290f2a722aca0f217daa1
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-09-19 13:58:39 +0200

    Formatting code

--- encyclopedia/util.py
+++ encyclopedia/util.py
@@ -15,7 +15,7 @@ def list_entries():
                 for filename in filenames if filename.endswith(".md")))
 
 
-def save_entry(title, content): 
+def save_entry(title, content):
     """
     Saves an encyclopedia entry, given its title and Markdown
     content. If an existing entry with the same title already exists,
@@ -38,56 +38,59 @@ def get_entry(title):
     except FileNotFoundError:
         return None
 
-def _get_name(name:str):
+
+def _get_name(name: str):
     """ If site name exists, returns that site name, otherwise, returns None """
 
-    result = [site_name for site_name in list_entries() if name.strip().lower()==site_name.lower()]
+    result = [site_name for site_name in list_entries(
+    ) if name.strip().lower() == site_name.lower()]
     return result[0] if result else None
 
-def _handle_search(request,all_sites=None):
+
+def _handle_search(request, all_sites=None):
     if not all_sites:
         all_sites = list_entries()
-    if request.method=='POST':
+    if request.method == 'POST':
         query = request.POST.get('q').strip().lower()
-        
+
         site_name = _get_name(query)
         if site_name:
             return HttpResponseRedirect(site_name)
         else:
-            simmilar_names = [name for name in all_sites if query in name.lower()]
-            return render(request,'encyclopedia/search_results.html',{
-                'simmilar_names':simmilar_names
-                })
+            simmilar_names = [
+                name for name in all_sites if query in name.lower()]
+            return render(request, 'encyclopedia/search_results.html', {
+                'simmilar_names': simmilar_names
+            })
 
 
 def markdown(content):
-    #return markdown2.markdown(content)
+    # return markdown2.markdown(content)
 
-    def replace(text,span,replacement):
+    def replace(text, span, replacement):
         return text[:span[0]]+replacement+text[span[1]:]
 
-    def handle_openclose(text,regex,open,close):
+    def handle_openclose(text, regex, open, close):
         p = re.compile(regex)
-        opening,changes=None,[]
-        
+        opening, changes = None, []
+
         for match in p.finditer(text):
             if opening is not None:
-                changes.append((opening.span(),open))
-                changes.append((match.span(),close))
-                
-                opening=None
+                changes.append((opening.span(), open))
+                changes.append((match.span(), close))
+                opening = None
             else:
                 opening = match
-        
+
         changes.reverse()
         for change in changes:
-            text=replace(text,*change)
-            
+            text = replace(text, *change)
+
         return text
-    
-    mdc = content # mdc - markdown content
-    
-    mdc = handle_openclose(mdc,r'\*\*','<b>','</b>')
-    mdc = handle_openclose(mdc,r'\*','<i>','</i>')
-    
+
+    mdc = content  # mdc - markdown content
+
+    mdc = handle_openclose(mdc, r'\*\*', '<b>', '</b>')
+    mdc = handle_openclose(mdc, r'\*', '<i>', '</i>')
+
     return mdc
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,85 +5,88 @@ from django.urls import reverse
 import random
 from . import util
 
+
 class CreatePageForm(forms.Form):
     name = forms.CharField(label='Page name')
-    content = forms.CharField(label='Page content', widget=forms.Textarea(attrs={'name':'page_content'}))
-    
-    
+    content = forms.CharField(
+        label='Page content', widget=forms.Textarea(attrs={'name': 'page_content'}))
+
+
 def index(request):
     all_sites = util.list_entries()
 
-    if request.method=='POST':
-        return util._handle_search(request,all_sites)
-        
+    if request.method == 'POST':
+        return util._handle_search(request, all_sites)
+
     return render(request, "encyclopedia/index.html", {
         "entries": all_sites
     })
 
-def content(request,content_name):
-    if request.method=='POST':
+
+def content(request, content_name):
+    if request.method == 'POST':
         return util._handle_search(request)
 
     content = util.get_entry(content_name)
     if not content:
-        return render(request,'encyclopedia/404_error.html',{
-            'content_name':content_name,
+        return render(request, 'encyclopedia/404_error.html', {
+            'content_name': content_name,
         })
     else:
         content_markdown = util.markdown(content)
 
-        return render(request,'encyclopedia/content.html',{
-            'content_name':content_name,
-            'content':content_markdown,
+        return render(request, 'encyclopedia/content.html', {
+            'content_name': content_name,
+            'content': content_markdown,
         })
-    
+
+
 def create_page(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = CreatePageForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
 
             if util._get_name(name):
-                return  render(request, 'encyclopedia/create_page.html', {
+                return render(request, 'encyclopedia/create_page.html', {
                     'create_page_form': form,
                     'error': 'A page with this name already exists.'
                 })
             else:
                 util.save_entry(name, form.cleaned_data['content'])
-                print("AA",name)
+                print("AA", name)
                 return HttpResponseRedirect(name)
 
-
-        return render(request,'encyclopedia/create_page.html',{
-        'create_page_form':form
+        return render(request, 'encyclopedia/create_page.html', {
+            'create_page_form': form
         })
 
-    return render(request,'encyclopedia/create_page.html',{
-        'create_page_form':CreatePageForm()
-        })
+    return render(request, 'encyclopedia/create_page.html', {
+        'create_page_form': CreatePageForm()
+    })
 
-def edit_page(request,content_name):
-    content_name_safe=util._get_name(content_name)   
-    
+
+def edit_page(request, content_name):
+    content_name_safe = util._get_name(content_name)
+
     if content_name_safe is None:
-        return render(request,'encyclopedia/404_error.html',{
-            'content_name':content_name,
+        return render(request, 'encyclopedia/404_error.html', {
+            'content_name': content_name,
         })
     del content_name
 
     content = util.get_entry(content_name_safe)
 
-    if request.method=='POST':
+    if request.method == 'POST':
         form = CreatePageForm(request.POST)
         if not form.is_valid():
-            return render(request,'encyclopedia/edit_page.html',{
-                'error':'Invalid input',
-                'create_page_form':form
+            return render(request, 'encyclopedia/edit_page.html', {
+                'error': 'Invalid input',
+                'create_page_form': form
             })
 
-        util.save_entry(content_name_safe,form.cleaned_data['content'])
-        return HttpResponseRedirect(reverse(f'content',args=[content_name_safe]))
-
+        util.save_entry(content_name_safe, form.cleaned_data['content'])
+        return HttpResponseRedirect(reverse(f'content', args=[content_name_safe]))
 
     initial_data = {
         'name': content_name_safe,
@@ -92,12 +95,11 @@ def edit_page(request,content_name):
 
     edit_form = CreatePageForm(initial=initial_data)
     edit_form.fields['name'].widget.attrs['readonly'] = 'readonly'
-    
 
-    return render(request,'encyclopedia/edit_page.html',{
-        'create_page_form':edit_form
-        })
+    return render(request, 'encyclopedia/edit_page.html', {
+        'create_page_form': edit_form
+    })
+
 
 def random_page(request):
-    return HttpResponseRedirect(reverse('content',args=[random.choice(util.list_entries())]))
-
+    return HttpResponseRedirect(reverse('content', args=[random.choice(util.list_entries())]))
