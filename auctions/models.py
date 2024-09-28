d2a67b448552fee8f7db48446bcd6a399042750d
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-09-28 14:57:41 +0200

    Added AuctionListing model fields

--- auctions/models.py
+++ auctions/models.py
@@ -1,13 +1,24 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class User(AbstractUser):
     pass
 
-class Auction(models.Model):
-    name = models.CharField(max_length=64)
-
+class AuctionListing(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=2048)
+    start_bid = models.DecimalField(max_digits=7,decimal_places=2,
+                                    verbose_name="Starting bid for auction(verbose)",
+                                    default=1,
+                                    validators=[
+                                        MaxValueValidator(99999.99),
+                                        MinValueValidator(1)
+                                    ])
+    img_url = models.URLField(max_length=64,verbose_name="Listing image url",blank=True,null=True)
+    category = models.CharField(max_length=64,verbose_name="Product category",blank=True,null=True)
+    
+    
 class Bid(models.Model):
     pass
 
