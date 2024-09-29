18b4fa27a2357ffc43307f2a49c28a66f254d966
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-09-29 02:02:30 +0200

    registered models

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User,AuctionListing,Bid,Comment,Category
+
+# Register your models here.
+admin.site.register(User)
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
+admin.site.register(Category)
