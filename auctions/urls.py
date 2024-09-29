95c6ec628e9e32bd3673ac7165e6e287336be07a
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-09-29 02:04:36 +0200

    add show_auction url

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create_listing"),
+    path("auctions/<int:auction_pk>", views.show_auction, name="auction"),
 
 ]
