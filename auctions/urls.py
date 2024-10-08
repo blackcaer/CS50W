fa3f18456eecf443be7c59e6b0d85554dcc2a1fc
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-10-08 15:24:24 +0200

    Added urls fot watchlist categories and category_listings

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create_auction", views.create_auction, name="create_auction"),
     path("auctions/<int:auction_pk>", views.show_auction, name="auction"),
+    path("watchlist", views.show_watchlist, name="watchlist"),
+    path("categories", views.show_categories, name="categories"),
+    path("categories/<str:category_name>", views.show_category_listings, name="category_listings"),
 
 ]
