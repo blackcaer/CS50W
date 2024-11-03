753bf45818defcfbd217f5edfde8776680918efe
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-11-03 15:43:13 +0100

    Added serializers

--- network/models.py
+++ network/models.py
@@ -6,6 +6,14 @@ class User(AbstractUser):
 
     following = models.ManyToManyField('self', symmetrical=False, related_name='followers')
 
+    def serialize(self):
+       
+        return {
+            "id": self.id,
+            "username": self.username
+        }
+    
+
     def __str__(self) -> str:
         return f"User {self.username} {self.email} (pk={self.pk})"
 
@@ -14,12 +22,20 @@ class Post(models.Model):
     content = models.CharField(max_length=2048)
     author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='posts')
     date_created = models.DateTimeField(auto_now_add=True)
-    users_liking = models.ManyToManyField(User,related_name='liked')
+    users_liking = models.ManyToManyField(User,related_name='liked_posts')
+
     def get_likes_count(self):
         return self.users_liking.count()
+    
+    def serialize(self):
+        users_liking = self.users_liking.all()
+        return {
+            "id": self.id,
+            "author": self.author.serialize(),
+            "users_liking": [user.username for user in users_liking],
+            "content": self.content,
+            "date_created": self.date_created.strftime("%b %d %Y, %I:%M %p"),
+        }
+    
     def __str__(self) -> str:
         return f"Post by {self.author.username} likes: {self.get_likes_count()} pk: {self.pk} '{self.content[:50]}'"
-
-
-
-#posts, likes, and followers.
