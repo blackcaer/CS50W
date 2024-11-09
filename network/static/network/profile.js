46a42080d7446701172cf77e7f2f4c3de8d02851
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-11-09 01:43:40 +0100

    handling follow button

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -1,10 +1,38 @@
 import * as base from './base.js';
 
 document.addEventListener('DOMContentLoaded', function () {
+    const followButton = document.querySelector("#follow-btn");
+
     base.fetch_user_posts(user_id)
         .then(posts => {
             base.show_posts(posts, '#user_posts_container');
         });
+    
+    if(followButton !== null)
+        handle_follow_btn(followButton)
+  
 });
 
+function handle_follow_btn(followButton)
+{
+    fetch(`/profile/${user_id}/is_followed`)
+    .then(response => response.json())
+    .then(data => {
+        followButton.innerHTML = data.is_followed ? "Unfollow" : "Follow";
+        followButton.style.visibility = "visible";
+    });
+
+    followButton.addEventListener("click", () => {
+        fetch(`/profile/${user_id}/toggle_follow`, {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": base.get_CRSF_token()
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            followButton.innerHTML = data.is_followed ? "Unfollow" : "Follow";
+        });
+    });  
+}
 
