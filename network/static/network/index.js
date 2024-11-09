22739be64f37835637330ee7b905c2a8d86b56d1
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-11-09 15:46:14 +0100

    create post handling, fetching new api route

--- network/static/network/index.js
+++ network/static/network/index.js
@@ -5,6 +5,36 @@ document.addEventListener('DOMContentLoaded', function () {
         .then(posts => {
             base.show_posts(posts, '#main_container', true);
         });
+
+
+    document.addEventListener('submit', async function (event) {
+        if (event.target.getAttribute('data-role') === 'create-post') {
+            event.preventDefault();
+            
+            const form = event.target
+            const formData = new FormData(form);
+            const content = formData.get('content');
+            
+            const response = await fetch('/add_post', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-CSRFToken': base.get_CRSF_token(),
+                },
+                body: JSON.stringify({ content: content })
+
+            });
+
+            if (response.ok) {
+                const newPost = await response.json();
+                document.querySelector('[data-role="posts-container"]').prepend(base.get_post_element(newPost));
+                form.querySelector('[name="content"]').value='';
+
+            } else {
+                console.error('Failed to create post:', response.statusText);
+            }
+        }
+    });
 });
 
 
