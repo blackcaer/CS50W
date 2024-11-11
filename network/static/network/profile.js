27b382ea634ed9829cd4e55b5568be3bb91bb06a
Kryspin Kucha <50886736+blackcaer@users.noreply.github.com>
2024-11-11 23:57:29 +0100

    added pagination handling to /profile

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -3,16 +3,40 @@ import * as base from './base.js';
 document.addEventListener('DOMContentLoaded', function () {
     const followButton = document.querySelector("#follow-btn");
 
-    base.fetch_user_posts(user_id, base.get_page_num())
-        .then(resp => {
-            const avalibe_pages_num = resp['page_count']
-            base.show_posts(resp['posts'], '#user_posts_container', false, avalibe_pages_num);
-        });
+    display_posts_on_site()
 
     if (followButton !== null)
         handle_follow_btn(followButton)
+       
+    document.addEventListener('click', async function (event) {
+        if (event.target.matches('.page-link')) {
+            await handle_pagination_btns(event);
+        }
+    });
 
 });
+
+function display_posts_on_site()
+{
+    base.fetch_user_posts(user_id, base.get_page_num())
+        .then(resp => {
+            document.querySelector('#user_posts_container').innerHTML='';
+            const avalibe_pages_num = resp['page_count']
+            base.show_posts(resp['posts'], '#user_posts_container', false, avalibe_pages_num);
+        });
+}
+
+async function handle_pagination_btns(event) {
+    event.preventDefault();
+    
+    const pageNum = base.get_clicked_pagination_btn(event)
+
+    const url = new URL(window.location.href);
+    url.searchParams.set('page', pageNum);
+    history.pushState(null, '', url);
+    
+    display_posts_on_site()
+}
 
 function handle_follow_btn(followButton) {
     fetch(`/profile/${user_id}/is_followed`)
